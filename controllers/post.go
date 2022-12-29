b9396d25e31c13acc8bcc58b7e1148050b81f916
karimdaw1991 <karimdaw91@gmail.com>
2022-12-29 23:18:52 +0100

    add createUserInput struct and function

--- controllers/post.go
+++ controllers/post.go
@@ -92,3 +92,21 @@ func DeleteAllPosts(c *gin.Context) {
 	models.DB.Delete(&posts)
 	c.JSON(http.StatusOK, gin.H{"data": "success"})
 }
+
+type CreateUserInput struct {
+	Name string `json:"name" binding:"required"`
+}
+
+func CreateUser(c *gin.Context) {
+	var input CreateUserInput
+	log.Print("creating user")
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	user := models.User{Name: input.Name}
+	user.SaveUser()
+
+	c.JSON(http.StatusOK, gin.H{"data": user})
+}
