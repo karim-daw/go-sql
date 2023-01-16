e892ba435120aefd82cc4f6379505cf23c7f4638
karimdaw1991 <karimdaw91@gmail.com>
2023-01-16 18:55:16 +0100

    commenting for debugging

--- models/material.go
+++ models/material.go
@@ -1,45 +1,44 @@
 package models
 
 type IMaterial interface {
-	getName() string
 	getDensity() float64
 	getThickness() float64
 	displayMaterialInfo() string
 }
 
-type IOpaqueMaterial interface {
-	getRValue() float64
+type TransperantMaterial interface {
+	setTransperancy(trans float64)
 }
 
-type ITransperantMaterial interface {
-	getTransperancy() float64
+type OpaqueMaterial interface {
+	setUValue(uVal float64)
 }
 
-type Material struct {
-	Name         string
-	Color        string
-	Density      float64
-	Thickness    float64
-	RValue       float64
-	Transperancy float64
-}
+// type Material struct {
+// 	Name         string
+// 	Color        string
+// 	Density      float64
+// 	Thickness    float64
+// 	RValue       float64
+// 	Transperancy float64
+// }
 
-func (m *Material) getName() string {
-	return m.Name
-}
+// func (m *Material) getName() string {
+// 	return m.Name
+// }
 
-func (m *Material) getDensity() float64 {
-	return m.Density
-}
+// func (m *Material) getDensity() float64 {
+// 	return m.Density
+// }
 
-func (m *Material) getThickness() float64 {
-	return m.Thickness
-}
+// func (m *Material) getThickness() float64 {
+// 	return m.Thickness
+// }
 
-func (m *Material) displayMaterialInfo() string {
-	return m.Name
-}
+// func (m *Material) displayMaterialInfo() string {
+// 	return m.Name
+// }
 
-func (m *Material) getRValue() float64 {
-	return m.RValue
-}
+// func (m *Material) getRValue() float64 {
+// 	return m.RValue
+// }
