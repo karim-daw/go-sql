0deb8577f96d1a381ec3fae1e850c9772eb748b7
karimdaw1991 <karimdaw91@gmail.com>
2023-01-16 18:55:05 +0100

    remove rotate

--- models/buildup.go
+++ models/buildup.go
@@ -1,9 +1,9 @@
 package models
 
-type IBuildup interface {
+type Buildup interface {
 	calculateVolume() float64
 	calculateArea() float64
 	calculateWeight() float64
-	rotate(angle float64)
 	calculateThickness() float64
+	calculateUValue() float64
 }
